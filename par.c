67208b0a492c768e34addacc54332b90ae23882c
AnaLidiaAgnus <analidiaagnus@hotmail.com>
2016-04-16 11:34:11 -0300

    Adicionando identificador de pares

---
+++ par.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.>
+int main(){
+    int numero;
+
+    printf("Digite um numero:");
+    scanf("%d",&numero);
+
+    if(numero % 2 ==0)
+      printf("O %d é par!", numero);
+
+}
